abbf5bd94cd7e2cfa6450f514ffa1c333055d02f
shofuratr <shofuratsabitah@gmail.com>
2025-03-08 01:50:00 +0000

    Revisi Total

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,12 @@
 # Import library
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
-import seaborn as sns
 
-st.title('Proyek Analisis Data: Bike Sharing')
+st.set_page_config(page_title='Bike Sharing Dashboard - Shofura')
+
+st.title('Bike Sharing Dashboard :bar_chart:')
 
 # Membaca dataset
 all_df = pd.read_csv('df_cleaned.csv') 
@@ -23,6 +23,10 @@ def create_daily_rent(df):
 def create_hour_rent(df):
     hour_rent = df.groupby('hr')['cnt'].mean().reset_index()
     return hour_rent
+
+def create_wind_rent(df):
+   wind_rent = df.groupby('wind_cluster')['cnt'].sum().reset_index()
+   return wind_rent
 
 # Membuat komponen filter
 min_date = all_df['dteday'].min()
@@ -47,52 +51,68 @@ main_df = all_df[(all_df['dteday'] >= str(start_date)) &
 monthly_rent = create_monthly_rent(main_df)
 daily_rent = create_daily_rent(main_df)
 hour_rent = create_hour_rent(main_df)
+wind_rent = create_wind_rent(main_df)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    total_user = main_df['cnt'].sum()
+    st.metric("Total All User", value=total_user)
+with col2:
+    total_casual = main_df['casual'].sum()
+    st.metric("Total Casual User", value=total_casual)
+with col3:
+    total_registered = main_df['registered'].sum()
+    st.metric("Total Registered User", value=total_registered)
 
 # Dashboard 1
-st.subheader("Tren Penyewaan Sepeda Berdasarkan Bulan Pada 2011 dan 2012")
-fig, ax = plt.subplots(figsize=(7, 5))
-sns.lineplot(
-    x='mnth',
-    y='cnt',
-    hue='yr',
-    data=monthly_rent,
-    marker="o",
-    palette="Spectral")
-plt.xlabel('Bulan')
-plt.ylabel('Jumlah Penyewaan')
-plt.legend(['2011', '2012'])
-st.pyplot(fig)
+st.subheader("Bagaimana tren penyewaan sepeda tiap bulannya pada tahun 2011 dan 2012?")
+fig1 = px.line(monthly_rent,
+               x='mnth',
+               y='cnt',
+               line_group='yr',
+               color='yr',
+               markers=True,
+               title="Tren Penyewaan Sepeda Berdasarkan Bulan Pada 2011 dan 2012").update_layout(xaxis_title='Bulan', yaxis_title='Jumlah Penyewaan')
+st.plotly_chart(fig1)
 
 # Dashboard 2
-st.subheader("Tren Penyewaan Harian Sepeda Casual vs Registered")
-fig2, ax = plt.subplots(figsize=(7, 5))
-sns.lineplot(
-    x='dteday',
-    y='casual',
-    label='casual',
-    color='red',
-    data=daily_rent)
-
-sns.lineplot(
-    x='dteday',
-    y='registered',
-    label='registered',
-    color='blue',
-    data=daily_rent)
-
-plt.xlabel('Hari')
-plt.ylabel('Jumlah Penyewaan')
-plt.legend()
-st.pyplot(fig2)
+st.subheader("Bagaimana perbandingan sewa sepeda pengguna casual vs registered pada tiap harinya?")
+fig2 = px.line(daily_rent,
+               x='dteday',
+               y=['casual','registered'],
+               title='Tren Penyewaan Harian Sepeda Casual vs Registered').update_layout(xaxis_title='Hari', yaxis_title='Jumlah Penyewaan')
+st.plotly_chart(fig2)
 
 # Dashboard 3
-st.subheader("Penyewaan Sepeda Berdasarkan Jam")
+st.subheader("Bagaimana tren penyewaan sepeda berdasarkan jam?")
 fig3 = px.line(hour_rent,
               x='hr',
               y='cnt',
               markers=True,
-              title='').update_layout(xaxis_title='Jam', yaxis_title='Total Penyewaan')
+              title='Penyewaan Sepeda Berdasarkan Jam').update_layout(xaxis_title='Jam', yaxis_title='Total Penyewaan')
 
+st.plotly_chart(fig3)
+
+# Dashboard 4
+st.subheader("Analisis Lanjutan : Windspeed Clustering")
+
+def wind_cluster(windspeed):
+  if windspeed < 0.3:
+    return 'Ringan'
+  elif windspeed < 0.6:
+    return 'Sedang'
+  else:
+    return 'Kuat'
+
+all_df['wind_cluster'] = all_df['windspeed'].apply(wind_cluster)
+
+fig3 = px.bar(wind_rent,
+              x='wind_cluster',
+              y='cnt',
+              color='wind_cluster',
+              color_discrete_map={'Kuat':'red','Ringan':'green','Sedang':'blue'},
+              title='Penyewaan Sepeda Berdasarkan Kecepatan Angin').update_layout(xaxis_title='Kategori Angin', yaxis_title='Jumlah Penyewaan')
 st.plotly_chart(fig3)
 
 st.caption('created by Shofura Tsabitah')
